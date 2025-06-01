648d788e934245f001156a757b795f56d65e11b7
bencruto60 <68362559+bencruto60@users.noreply.github.com>
2025-06-01 08:54:08 +0800

    Fixed Render Deploy2

--- FlaskApp.py
+++ FlaskApp.py
@@ -138,5 +138,5 @@ def history():
         return jsonify({'error': 'Failed to fetch history'}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
